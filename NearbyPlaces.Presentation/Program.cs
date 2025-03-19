6c27f52abef241c2b5086e29dda8027054169d71
Erdem <erdemsincer123@icloud.com>
2025-03-19 04:43:46 +0300

    n

--- NearbyPlaces.Presentation/Program.cs
+++ NearbyPlaces.Presentation/Program.cs
@@ -4,10 +4,12 @@ using NearbyPlaces.Infrastructure.DependencyInjection;
 using NearbyPlaces.Application.Common;
 using MediatR;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddInfrastructureServices();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
